dc1f738cf765cedada5705d2607088c6e1af3c08
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-02 17:22:02 -0700

    Styled header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import styles from './styles.module.scss';
+
 export function Header() {
   return (
-    <header>
-      <div>
-        <img src="/imaegs/logo.svg" alt="ig.news" />
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
       </div>

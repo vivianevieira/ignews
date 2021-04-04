0800cd58a18cba488db4a3291083aabd4cfe3842
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-03 17:55:19 -0700

    Added signIn and signOut onclick functionality to login button and created a provider for session information.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession} from 'next-auth/client'
+import { signIn, signOut, useSession} from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
   const [session] = useSession();
 
+  console.log(session);
+
   return  session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Viviane Vieira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (

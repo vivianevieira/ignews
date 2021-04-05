ffce32121fdd041fd09997801edb4325072ecb34
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-04 20:57:29 -0700

    Created a customer on stripe using users email.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +6,23 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+
+  function handleSubsccribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubsccribe}
     >
       Subscribe now
     </button>

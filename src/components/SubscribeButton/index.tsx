aa7deedde1fa2f882659f4dbbe194e2b75ffe0d2
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-05 09:49:26 -0700

    Redirected users to Stripe checkout page upon clicking on Subscribe now button.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -9,13 +11,23 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
 
-  function handleSubsccribe() {
+  async function handleSubsccribe() {
     if (!session) {
       signIn('github')
       return;
     }
 
+    try {
+      const response = await api.post('/subscribe')
 
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (

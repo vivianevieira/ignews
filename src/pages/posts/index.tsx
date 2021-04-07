704b533178b6680196b14acecd9151a3889b9b33
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-07 15:44:19 -0700

    Mapped through the posts to include them on the posts index page.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,25 @@
 import Head from 'next/head';
-import { getPrismicClient } from '../../services/prismic';
-import Prismic from '@prismicio/client';
-
-import styles from './styles.module.scss';
 import { GetStaticProps } from 'next';
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import styles from './styles.module.scss';
+
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
     <Head>
@@ -14,21 +28,13 @@ export default function Posts() {
 
     <main className={styles.container}>
       <div className={styles.posts}>
-        <a href="#">
-          <time>April 7, 2021</time>
-          <strong>Advice From a Hiring Manager to a Code Bootcamp Graduate</strong>
-          <p>As a director of engineering responsible for a lot of hiring and recruiting, I often talk with folks who have recently graduated from a 3–6 month code bootcamp.</p>
+        { posts.map(post => (
+          <a key={post.slug} href="#">
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
         </a>
-        <a href="#">
-          <time>April 7, 2021</time>
-          <strong>Advice From a Hiring Manager to a Code Bootcamp Graduate</strong>
-          <p>As a director of engineering responsible for a lot of hiring and recruiting, I often talk with folks who have recently graduated from a 3–6 month code bootcamp.</p>
-        </a>
-        <a href="#">
-          <time>April 7, 2021</time>
-          <strong>Advice From a Hiring Manager to a Code Bootcamp Graduate</strong>
-          <p>As a director of engineering responsible for a lot of hiring and recruiting, I often talk with folks who have recently graduated from a 3–6 month code bootcamp.</p>
-        </a>
+        ))}
       </div>
     </main>
     </>
@@ -45,9 +51,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+ const posts = response.results.map(post => {
+  return {
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('en-US', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+ })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }

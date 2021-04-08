273c22c4b2fb2e45d574a4e9f876f21d661bb902
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-08 15:33:49 -0700

    Created and styled Post page for each blogl post.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
@@ -29,11 +30,13 @@ export default function Posts({ posts }: PostsProps) {
     <main className={styles.container}>
       <div className={styles.posts}>
         { posts.map(post => (
-          <a key={post.slug} href="#">
-            <time>{post.updatedAt}</time>
-            <strong>{post.title}</strong>
-            <p>{post.excerpt}</p>
-        </a>
+          <Link href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          </Link>
         ))}
       </div>
     </main>

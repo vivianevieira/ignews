ea9591866947b834c4a56ec0edbc02086f149a93
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-09 19:34:05 -0700

    Checked for activeSubscription status in order to show preview of post according to this status.

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,11 +1,13 @@
 import { GetStaticProps } from "next"
 import Link from 'next/link'
-import { getSession } from "next-auth/client"
+import { useSession } from "next-auth/client"
+import { useRouter } from 'next/router';
 import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../../services/prismic";
 
 import styles from '../post.module.scss';
+import { useEffect } from "react";
 
 interface PostPreviewProps {
   post: {
@@ -17,6 +19,15 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [ session ] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session])
+
   return (
     <>
       <Head>
